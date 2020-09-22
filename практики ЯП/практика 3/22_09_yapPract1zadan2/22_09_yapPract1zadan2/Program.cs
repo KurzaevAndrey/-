b37253f3fea60588a106ef1cb9663bb48608b9a3
KurzaevAndrey <71704057+KurzaevAndrey@users.noreply.github.com>
2020-09-22 14:50:36 +0300

    Add files via upload

---
+++ практики ЯП/практика 3/22_09_yapPract1zadan2/22_09_yapPract1zadan2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System;
+using System.Text;
+namespace _22_09_yapPract1zadan2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StreamReader sr = new StreamReader(@"boot.ini");
+      StreamWriter sw = new StreamWriter("boot - utf7.txt", false, Encoding.UTF7);
+      sw.WriteLine(sr.ReadToEnd());
+      sw.Close();
+      sr.Close();
+
+
+    }
+  }
+}
