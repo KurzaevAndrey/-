73bd95da0edef71de381143eac0e92a0741233b9
KurzaevAndrey <71704057+KurzaevAndrey@users.noreply.github.com>
2020-10-20 15:35:05 +0300

    Add files via upload

---
+++ практики ЯП/Практика 8/SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+namespace SimpleThreadingDemo
+{
+  class Program
+  {
+    static void counting()
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine("Count: " + i + " - Thread " + Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(10);
+      } 
+    }
+    static void Main(string[] args)
+    {
+      ThreadStart starter = new ThreadStart(counting);
+      Thread first = new Thread(starter);
+      Thread second = new Thread(starter);
+      first.Start(); 
+      second.Start();
+      first.Join(); 
+      second.Join();
+     // Console.Read();
+
+      
+    }
+  }
+}
