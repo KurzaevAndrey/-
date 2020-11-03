9b25106e88b9a066fb7c0ee053b622c908191988
KurzaevAndrey <71704057+KurzaevAndrey@users.noreply.github.com>
2020-11-03 15:37:06 +0300

    Add files via upload

---
+++ практики ЯП/Практика 9/second_application/second_application/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace second_application
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.загрузкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.загрузкаИзображенияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.pNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.jPGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+      this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+      this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.toolStrip1.SuspendLayout();
+      this.menuStrip1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // toolStrip1
+      // 
+      this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+      this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+      this.toolStrip1.Name = "toolStrip1";
+      this.toolStrip1.Size = new System.Drawing.Size(45, 426);
+      this.toolStrip1.TabIndex = 0;
+      this.toolStrip1.Text = "menu";
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.загрузкаToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+      this.menuStrip1.TabIndex = 1;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // fileToolStripMenuItem
+      // 
+      this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+      this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+      this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+      this.fileToolStripMenuItem.Text = "file";
+      this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+      // 
+      // загрузкаToolStripMenuItem
+      // 
+      this.загрузкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузкаИзображенияToolStripMenuItem});
+      this.загрузкаToolStripMenuItem.Name = "загрузкаToolStripMenuItem";
+      this.загрузкаToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+      this.загрузкаToolStripMenuItem.Text = "Загрузка";
+      // 
+      // загрузкаИзображенияToolStripMenuItem
+      // 
+      this.загрузкаИзображенияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pNGToolStripMenuItem,
+            this.jPGToolStripMenuItem});
+      this.загрузкаИзображенияToolStripMenuItem.Name = "загрузкаИзображенияToolStripMenuItem";
+      this.загрузкаИзображенияToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+      this.загрузкаИзображенияToolStripMenuItem.Text = "Загрузка изображения";
+      // 
+      // pNGToolStripMenuItem
+      // 
+      this.pNGToolStripMenuItem.Name = "pNGToolStripMenuItem";
+      this.pNGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.pNGToolStripMenuItem.Text = "PNG";
+      this.pNGToolStripMenuItem.Click += new System.EventHandler(this.pNGToolStripMenuItem_Click);
+      // 
+      // jPGToolStripMenuItem
+      // 
+      this.jPGToolStripMenuItem.Name = "jPGToolStripMenuItem";
+      this.jPGToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.jPGToolStripMenuItem.Text = "JPG";
+      this.jPGToolStripMenuItem.Click += new System.EventHandler(this.jPGToolStripMenuItem_Click);
+      // 
+      // выходToolStripMenuItem
+      // 
+      this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+      this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+      this.выходToolStripMenuItem.Text = "Выход";
+      this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+      // 
+      // toolStripButton1
+      // 
+      this.toolStripButton1.AutoSize = false;
+      this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+      this.toolStripButton1.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+      this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButton1.Name = "toolStripButton1";
+      this.toolStripButton1.Size = new System.Drawing.Size(44, 44);
+      this.toolStripButton1.Text = "toolStripButton1";
+      this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+      // 
+      // toolStripButton2
+      // 
+      this.toolStripButton2.AutoSize = false;
+      this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+      this.toolStripButton2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+      this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButton2.Name = "toolStripButton2";
+      this.toolStripButton2.Size = new System.Drawing.Size(44, 44);
+      this.toolStripButton2.Text = "toolStripButton2";
+      this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+      // 
+      // toolStripButton3
+      // 
+      this.toolStripButton3.AutoSize = false;
+      this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+      this.toolStripButton3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+      this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButton3.Name = "toolStripButton3";
+      this.toolStripButton3.Size = new System.Drawing.Size(44, 44);
+      this.toolStripButton3.Text = "toolStripButton3";
+      this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(96, 27);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(704, 423);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.toolStrip1);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.toolStrip1.ResumeLayout(false);
+      this.toolStrip1.PerformLayout();
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузкаИзображенияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pNGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jPGToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ практики ЯП/Практика 9/second_application/second_application/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace second_application
+{
+  public partial class Form1 : Form
+  {
+    Image MemForImage;
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      DialogResult rsl = MessageBox.Show("Вы действительно хотите выйти из приложения?", "Внимание!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+      // если пользователь нажал кнопку да 
+      if (rsl == DialogResult.Yes)
+      {// выходим из приложения 
+        Application.Exit();
+
+      }
+    }
+
+    private void toolStripButton2_Click(object sender, EventArgs e)
+    {
+      LoadImage(true);
+    }
+
+    private void toolStripButton1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void toolStripButton3_Click(object sender, EventArgs e)
+    {
+      LoadImage(false);
+    }
+    private void LoadImage(bool jpg)
+    {
+
+      // директория, которая будет выбрана как начальная в окне для выбора файла 
+      openFileDialog1.InitialDirectory = "c:";
+
+      //еслибудемвыбирать jpg файлы
+      if (jpg)
+      {
+        // устанавливаем формат файлов для загрузки - jpg
+        openFileDialog1.Filter = "image (JPEG) files (*.jpg)|*.jpg|All files (*.*)|*.*";
+      }
+      else
+      {
+        // устанавливаем формат файлов для загрузки - png
+        openFileDialog1.Filter = "image (PNG) files (*.png)|*.png|All files (*.*)|*.*";
+      }
+
+      // если открытие окна выбора файла завершилось выбором файла и нажатием кнопки ОК 
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+
+        try// безопасная попытка 
+        {
+          // пытаемся загрузить файл с именем openFileDialog1.FileName - выбранный пользователем файл. 
+          MemForImage = Image.FromFile(openFileDialog1.FileName);
+          // устанавливаем картинку в поле элемента PictureBox
+          pictureBox1.Image = MemForImage;
+        }
+        catch (Exception ex)// еслипопытказагрузкинеудалась
+        {
+          // выводим сообщение с причиной ошибки 
+          MessageBox.Show("Не удалось загрузить файл: " + ex.Message);
+        }
+
+      }
+
+    }
+
+    private void jPGToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      LoadImage(true);
+    }
+
+    private void pNGToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      LoadImage(false);
+    }
+
+    private void pictureBox1_Click(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
